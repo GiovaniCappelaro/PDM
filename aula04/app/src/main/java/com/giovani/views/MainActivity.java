b2438324a6732f19ff6acf74d97e5e80924ad07c
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-08-17 19:59:38 -0300

    Criação ActivityMainBinding

--- aula04/app/src/main/java/com/giovani/views/MainActivity.java
+++ aula04/app/src/main/java/com/giovani/views/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.giovani.views.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Referência para a classe ActivityMainBinding, criando um objeto do seu tipo
+    private ActivityMainBinding amb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Começa a lógica a partir daqui após iniciar a ActivityMainBinding
+
+        //instanciar a classe activity
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+
+
+        setContentView(R.layout.activity_main); //chama meu layout activity_main
     }
 }
