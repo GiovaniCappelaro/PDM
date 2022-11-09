ae290dacbe88c1913f86c3d2aad53859a304e6b4
GiovaniCappelaro <giovani.c@aluno.ifsp.edu.br>
2022-11-09 19:19:23 -0300

    atualização do projeto semanal myContacts ANTES DE PERSISTENCIA

--- 00-PROJETO_SENDMAIL_SEMANAL/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/view/ContactActivity.kt
+++ 00-PROJETO_SENDMAIL_SEMANAL/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/view/ContactActivity.kt
@@ -3,8 +3,10 @@ package br.edu.ifsp.ads.pdm.mycontacts.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.edu.ifsp.ads.pdm.mycontacts.databinding.ActivityContactBinding
 import br.edu.ifsp.ads.pdm.mycontacts.model.Constant.EXTRA_CONTACT
+import br.edu.ifsp.ads.pdm.mycontacts.model.Constant.VIEW_CONTACT
 import br.edu.ifsp.ads.pdm.mycontacts.model.Contact
 import kotlin.random.Random
 
@@ -16,9 +18,31 @@ class ContactActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(acb.root)
 
+        val recievedContact = intent.getParcelableExtra<Contact>(EXTRA_CONTACT)
+
+        recievedContact?.let { _recievedContact ->
+            with(acb){
+                nameEt.setText(_recievedContact.name)
+                addressEt.setText(_recievedContact.address)
+                phoneEt.setText(_recievedContact.phone)
+                emailEt.setText(_recievedContact.email)
+            }
+
+        } //_recievedContact é o objeto de contato em si
+        //intent pra visualizar o contato:
+        val viewContact = intent.getBooleanExtra(VIEW_CONTACT, false)
+        if(viewContact){
+            acb.nameEt.isEnabled = false
+            acb.addressEt.isEnabled = false
+            acb.phoneEt.isEnabled = false
+            acb.emailEt.isEnabled = false
+
+            acb.saveBt.visibility = View.GONE  //esconde o botão de salvar por n precisar dele aqui
+        }
+
         acb.saveBt.setOnClickListener {
             val contact = Contact(
-                id = Random(System.currentTimeMillis()).nextInt(),
+                id = recievedContact?.id?: Random(System.currentTimeMillis()).nextInt(),
                 name = acb.nameEt.text.toString(),
                 address = acb.addressEt.text.toString(),
                 phone = acb.phoneEt.text.toString(),
--- 00-PROJETO_SENDMAIL_SEMANAL/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/view/MainActivity.kt
+++ 00-PROJETO_SENDMAIL_SEMANAL/app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/view/MainActivity.kt
@@ -2,8 +2,12 @@ package br.edu.ifsp.ads.pdm.mycontacts.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.AdapterContextMenuInfo
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +15,7 @@ import br.edu.ifsp.ads.pdm.mycontacts.R
 import br.edu.ifsp.ads.pdm.mycontacts.adapter.ContactAdapter
 import br.edu.ifsp.ads.pdm.mycontacts.databinding.ActivityMainBinding
 import br.edu.ifsp.ads.pdm.mycontacts.model.Constant.EXTRA_CONTACT
+import br.edu.ifsp.ads.pdm.mycontacts.model.Constant.VIEW_CONTACT
 import br.edu.ifsp.ads.pdm.mycontacts.model.Contact
 
 class MainActivity : AppCompatActivity() {
@@ -40,12 +45,43 @@ class MainActivity : AppCompatActivity() {
             if (result.resultCode == RESULT_OK) {
                 val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
                 contact?.let { _contact->
-                    contactList.add(_contact)
-                    contactAdapter.notifyDataSetChanged()
+                    //verificar se o contato ja n existe antes de inserir na lista:
+                    if(contactList.any{it.id == _contact.id}){
+                        //Alterar o contato na mesma posição
+                        val position = contactList.indexOfFirst { it.id == _contact.id }
+                        contactList[position] = _contact
+                    }
+                    else{
+                        //inserir novo contato no fim da lista
+                        contactList.add(_contact)
+                    }
+
+                    contactAdapter.notifyDataSetChanged() //avisa pro adapter que houveram mudanças no data source
                 }
             }
         }
+
+        registerForContextMenu(amb.contactsLv) //vincular a listView pra funcionar o contextMenu
+
+        //listener pra click rapido:
+        amb.contactsLv.onItemClickListener = object: AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val contact = contactList[position]
+                //intent pra pegar o contato pelo clik rapido e mandar pra lista:
+                val contactIntent = Intent(this@MainActivity, ContactActivity::class.java)
+                contactIntent.putExtra(EXTRA_CONTACT, contact)
+                contactIntent.putExtra(VIEW_CONTACT, true)
+                startActivity(contactIntent)
+            }
+        }
     }
+
+    //metodos tratar do menu
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -61,6 +97,36 @@ class MainActivity : AppCompatActivity() {
             else -> { false }
         }
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context_menu_main, menu) //inflar o menu de contexto(aparece ao segurar)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val position = (item.menuInfo as AdapterContextMenuInfo ).position //pegar posição do item do menu
+        return when(item.itemId){
+            R.id.removeContactMi -> {
+                //remove o contato da lista:
+                contactList.removeAt(position)
+                contactAdapter.notifyDataSetChanged()  //avisar o data source da exclusão
+                true
+            }
+            R.id.editContactMi ->{
+                val contact = contactList[position]
+                val contactIntent = Intent(this, ContactActivity::class.java)
+                contactIntent.putExtra(EXTRA_CONTACT, contact)
+                contactIntent.putExtra(VIEW_CONTACT, false)
+                carl.launch(contactIntent)
+                true
+            }
+            else -> {false}
+        }
+    }
+
 
     private fun fillContactList() {
         for (i in 1..10) {
